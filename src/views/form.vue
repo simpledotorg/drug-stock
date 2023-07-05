b30c8d1ea83d4abf24462aa7ad078b19066f02ad
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2023-07-05 15:14:09 +0100

    form updates
    - changes to batching with slots
    - mapping v-models
    - select options
    - generate monthyear list
    - populate dropdowns

--- src/views/form.vue
+++ src/views/form.vue
@@ -1,32 +1,83 @@
 <template>
-  <div class="w-full max-w-lg m-auto bg-white p-8 shadow-md rounded-md">
+  <div class="w-full max-w-lg m-auto bg-white p-8 md:shadow-md md:rounded-md">
     <h1 class=" text-3xl mb-6 font-extrabold">Drug stock form</h1>
     <form v-if="formOptions.facilities.length > 0">
-      <label class="block text-lg font-bold mb-2" for="">Facility name</label>
-      <select name="" id="" class="mb-6">
-        <option v-for="facility in formOptions.facilities" value="">{{ facility }}</option>
+      <label class="block text-xl font-medium mb-2" for="">Facility name</label>
+      <select name="" id="" class="uppercase" v-model="rawFormFieldsData.facility">
+        <option value="" disabled selected>Select facility</option>
+        <option v-for="facility in formOptions.facilities" :value="facility">{{ facility }}</option>
       </select>
-      <div v-for="drug in formOptions.drugs" class="border-t border-slate-300 py-6">
+      <label class="block text-xl font-medium mb-2" for="">For end of month</label>
+      <select name="" id="" class="uppercase" v-model="rawFormFieldsData.monthyear">
+        <option v-for="(monthyear, index) in monthyearArray()" :key="index" :value="monthyear">{{ monthyear }}</option>
+      </select>
+
+      <div v-for="drug in formOptions.drugs" class="border-t border-slate-100 py-6">
         <h2 class="text-xl font-bold">{{ drug }}</h2>
-        <FormBatch :batchNumber="batch" v-for="batch in formOptions.drugBatches[drug]"></FormBatch>
-        <button v-if="formOptions.drugBatches[drug] < 3" @click.prevent="formOptions.drugBatches[drug] < 3 ? formOptions.drugBatches[drug] += 1 : undefined"
-          class="uppercase text-xs font-medium mt-6 p-1.5 px-3 rounded-md bg-sky-800 text-white">add
-          another batch</button>
-        <p v-else
-          class=" inline-block uppercase text-xs font-medium mt-6 p-1.5 px-3 rounded-md border border-slate-300">max batches</p>
+        <FormBatch :batchNumber="batch" v-for="(batch, index) in formOptions.drugBatches[drug]">
+          <template v-slot:stock>
+            <input type="number" class="mt-2 mb-1" v-model="rawFormFieldsData[drug + ' b' + (index+1) + ' stock']">
+          </template>
+          <template v-slot:expiry>
+            <input type="date" class="mt-2 mb-1" v-model="rawFormFieldsData[drug + ' b' + (index+1) + ' expiry']">
+          </template>
+        </FormBatch>
+        <button v-if="formOptions.drugBatches[drug] < 3"
+          @click.prevent="formOptions.drugBatches[drug] < 3 ? formOptions.drugBatches[drug] += 1 : undefined"
+          class="uppercase text-sm font-medium mt-3 py-2 rounded-md text-[#0075EB]">
+          + add batch
+        </button>
+        <div v-else>
+          <p class="uppercase text-sm font-medium mt-3 py-2 text-neutral-600">
+            max three batches per medicine</p>
+          <p class="text-sm text-neutral-500 mt-2">If you have more than 3 batches then add any additional batch stocks to
+            batch 3 using the same date</p>
+        </div>
       </div>
-      <button @click.prevent="postSheetsData({ data: { value1: 1, value: 2 } })">
-        submit
+      <!-- <div class="bg-[#F3F9FF] w-full p-8"> -->
+      <button
+        class="w-full p-3 shadow-lg border-b-2 border-blue-800 bg-blue-500 rounded-md text-lg uppercase text-white tracking-wider"
+        @click.prevent="postData()">
+        submit drug stock
       </button>
+      <!-- </div> -->
     </form>
+    <div v-else class="py-10">
+      <Loading></Loading>
+      <p class="text-center uppercase text-sm text-slate-400 font-bold mt-5">loading</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+
 import getSheetsData from "../utils/getSheetsData"
 import postSheetsData from "../utils/postData"
+
 import FormBatch from "../components/form/batch.vue"
-import { onMounted, ref } from "vue";
+import Loading from "../components/loading.vue"
+
+interface drugBatch {
+  stock: number;
+  expiry: Date;
+}
+
+interface drugBatches {
+  1: drugBatch;
+  2: drugBatch;
+  3: drugBatch;
+}
+
+interface drugFormObj {
+  [key: string]: drugBatches;
+}
+
+interface formOptions {
+  facility: string;
+  monthyear: string;
+  drugs: drugFormObj;
+}
 
 const formOptions = ref<{ facilities: string[], drugs: string[], drugBatches: { [key: string]: number } }>({ facilities: [], drugs: [], drugBatches: {} })
 onMounted(async () => {
@@ -34,6 +85,11 @@ onMounted(async () => {
   formOptions.value.facilities = data.facilities
   formOptions.value.drugs = data.drugs
   createObjectForBatches()
+  console.log(data);
+
+  //build rawfields object
+  rawFormFieldsData.value = convertArrayToObject(data.rawFormFields)
+  rawFormFieldsData.value.monthyear = monthyearArray()[0]
 })
 
 function createObjectForBatches() {
@@ -44,11 +100,56 @@ function createObjectForBatches() {
   formOptions.value.drugBatches = object;
 }
 
+const rawFormFieldsData = ref<{}>({})
+function convertArrayToObject(arr) {
+  return arr.reduce(function (obj, item) {
+    obj[item] = '';
+    return obj;
+  }, {});
+}
+function monthyearArray() {
+  var months = [];
+  var currentDate = new Date();
 
+  for (var i = 0; i < 12; i++) {
+    var month = currentDate.toLocaleString('default', { month: 'short' }).toLowerCase();
+    var year = currentDate.getFullYear();
+    var formattedDate = month + '-' + year;
+    months.push(formattedDate);
+
+    currentDate.setMonth(currentDate.getMonth() - 1); // Move to the previous month
+  }
+
+  return months;
+}
+
+function postData() {
+  console.log(new Date);
+  console.log(rawFormFieldsData.value.facility);
+  
+  if (rawFormFieldsData.value.facility === '') {
+    console.log('no facility selected');
+    return
+  }
+  // rawFormFieldsData.timestamp.value = Date
+  const postData = { rowData: convertObjectValuesToArray(rawFormFieldsData.value) };
+  console.log(postData);
+  
+  // return
+  postSheetsData(postData)
+}
+
+function convertObjectValuesToArray(obj) {
+  return Object.values(obj);
+}
 </script>
 
 <style>
-input,
+input {
+  @apply bg-gray-100/75 p-4 px-5 shadow-inner rounded w-full;
+}
+
 select {
-  @apply bg-gray-100 p-3 px-4 shadow-inner rounded w-full;
-}</style>
+  @apply rounded w-full mb-6 p-4 text-[#0075EB] bg-[#E0F0FF] border-b-[3px] border-blue-200 shadow-sm tracking-wider;
+}
+</style>
