fb17fe9f35b81388377868cd3b0b0a149cb5c613
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2023-07-05 15:12:44 +0100

    getSheetsData updates

--- src/utils/getSheetsData.ts
+++ src/utils/getSheetsData.ts
@@ -1,6 +1,7 @@
 export default async function getData(): Promise<{
   facilities: string[];
   drugs: string[];
+  rawFormFields: string[];
 }> {
   const response = await fetch(
     "https://script.google.com/macros/s/AKfycbzhZvb85DvQf37xZopLurDFjzwlIdWMxkcPJN9yFUu2E7lga3c-7s-bu3EYHGsgJ4n7/exec"
@@ -8,10 +9,14 @@ export default async function getData(): Promise<{
   const rawData = await response.json();
   // console.log(rawData.data);
   
-  const { 'Facility name': facilities, 'Drug': drugs } = rawData.data;
+  const { 'Facility name': facilities, 'Drug': drugs, formFields: rawFormFields } = rawData.data;
+
+  const drugsList = createDrugList(rawFormFields)
+
   return {
     facilities,
-    drugs,
+    drugs: drugsList,
+    rawFormFields,
   };
   // const rawData = JSON.parse(response)
   // .then((response) => response.text())
@@ -23,4 +28,15 @@ export default async function getData(): Promise<{
   //   console.error("Error:", error);
   // });
   // return data;
+}
+
+function createDrugList(rawFormFields: string[]) {
+  const editedArray: string[] = []
+  rawFormFields.forEach(field => {
+    editedArray.push(field.replace('b1', '').replace('b2', '').replace('b3', '').replace('stock', '').replace('expiry', '').trim())
+  })
+  const filterToDrugs = editedArray.filter(drug => drug.includes('mg')) 
+  const drugsList = [...new Set(filterToDrugs)]
+  console.log(drugsList);
+  return drugsList;
 }
