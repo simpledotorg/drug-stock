01a92393949ca76839519ab014039c997c0b7b37
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2023-07-05 15:11:28 +0100

    us hashing router for gh pages
    - also change /about to /form

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/home.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
@@ -11,7 +11,7 @@ const router = createRouter({
     },
     {
       path: "/form",
-      name: "about",
+      name: "form",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
