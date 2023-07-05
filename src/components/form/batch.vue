5d1641e91add25b212d184b24859f154fa5bacc3
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2023-07-05 15:10:52 +0100

    convert batch component to a <slot>
    - use slots for the inputs for easier data mapping with v-model

--- src/components/form/batch.vue
+++ src/components/form/batch.vue
@@ -2,12 +2,12 @@
     <p class="text-lg mt-3">Batch {{ batchNumber }}</p>
     <div class="flex gap-6">
         <div class="w-1/2">
-            <label class="block my-2" for="">Stock on hand</label>
-            <input type="number">
+            <slot name="stock"></slot>
+            <label class="block text-sm text-neutral-500" for="">Stock on hand</label>
         </div>
         <div class="w-1/2">
-            <label class="block my-2" for="">Expiry date</label>
-            <input type="date">
+            <slot name="expiry"></slot>
+            <label class="block text-sm text-neutral-500" for="">Expiry date</label>
         </div>
     </div>
 </template>
